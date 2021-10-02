b6b710f9597857adcf1922ec7830e2308d651c4e
Anar <78043093+PP1-hub@users.noreply.github.com>
2021-10-02 14:26:23 +0600

    Add files via upload

--- Lab 3.sql
+++ Lab 3.sql
@@ -59,27 +59,40 @@ WHERE name not in (SELECT name
 
 --TASK 3
 
-SELECT name, ID, title                                       --a
+SELECT name, student.ID, title, takes.grade, course.course_id                                       --a
 FROM student, takes, course
-WHERE student.ID = takes.ID and takes.course_id = course.course_id and course.dept_name = 'Comp. Sci.' and takes.grade in ('A', 'A-')
-GROUP BY asc
+WHERE student.ID = takes.ID and takes.course_id = course.course_id and course.dept_name = 'Comp. Sci.'
+  and takes.grade in ('A', 'A-')
+ORDER BY name;
 
-SELECT name, s_ID, i_ID                                      --b
-FROM advisor, student, takes
-WHERE takes.id= advisor.s_ID and student.id = takes.id and takes.grades not in('A', 'A-', 'B+', 'B')
+SELECT student.name, instructor.name,  student.ID, instructor.dept_name, takes.course_id, takes.grade       --b
+FROM instructor, advisor, student, takes
+WHERE student.ID = advisor.s_ID  and student.id = takes.id
+  and instructor.ID = advisor.s_ID
+  and takes.grades not in('A', 'A-', 'B+', 'B')
 
-SELECT dept_name                                             --c
-FROM course
-WHERE dept_name not in (SELECT dept_name
-                        FROM course, takes
-                        WHERE course.course_id = takes.course_id and takes.grade in ('F', 'C'));
+SELECT department. dept_name                                             --c
+FROM department, student, takes
+WHERE department.dept_name = student.dept_name
+          and student.ID = takes.ID
+          and department.dept_name not in (SELECT department.dept_name
+                        FROM department, student, takes
+                        WHERE student.ID = takes.ID
+                          and department.dept_name = student.dept_name
+                          and takes.grade in ('F', 'C'));
 
-SELECT DISTINCT name                                                   --d
-FROM instructor
-WHERE name not in( SELECT name
-                   FROM takes, teaches, instructor
-                   WHERE instructor.ID= teaches.ID and teaches.course_id = takes.course_id and takes.grade = 'A');
+SELECT  instructor.name, takes.ID, takes.course_id, takes.grade            --d
+FROM instructor,takes, course, advisor
+WHERE advisor.s_ID = takes.ID and instructor.ID = advisor.s_ID
+      and course.course_id = takes.course_id
+       and instructor.name not in( SELECT instructor.name
+                   FROM takes, course, instructor, advisor
+                   WHERE instructor.ID= advisor.i_ID and course.course_id = takes.course_id
+                     and advisor.s_ID = takes.ID
+                     and takes.grade = 'A');
 
-SELECT DISTINCT course_id, title, start_hr, start_min, end_hr, end_min  --e
+SELECT time_slot.day, course.title, time_slot.start_hr, time_slot.start_min, time_slot.end_hr, time_slot.end_min  --e
 FROM time_slot, course, section
-WHERE course.course_id = section.course_id and section.time_slot_id = time_slot.time_slot_id and end_hr < 13;
+WHERE course.course_id = section.course_id and section.time_slot_id = time_slot.time_slot_id
+  and time_slot.end_hr <= 12 and time_slot.end_min <= 59
+ORDER BY end_hr
